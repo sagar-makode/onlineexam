3af15518d7a6839c6203a12bceab7014bfdbc1d4
BalrajMaurya <balrajmaurya786@gmail.com>
2024-03-21 17:24:57 +0530

    [CSS-Design] signIn and signUp animation pending

--- src/UserSignIn/SignIn.js
+++ src/UserSignIn/SignIn.js
@@ -1,7 +1,10 @@
 // SigninForm.js
+import "./SignIn.css"
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { StudentsigninRequest, TeachersigninRequest, signinRequest } from '../actions/userActions';
+import { StudentsigninRequest, TeachersigninRequest } from '../actions/userActions';
+import SideImage from "../assets/8.png"
+import { Link } from 'react-router-dom'
 
 
 const SignIn = () => {
@@ -11,10 +14,10 @@ const SignIn = () => {
     password: '',
     isAdmin: false
   });
-
+  const[admin,setAdmin]=useState(false)
   console.log(formData);
 
-  
+
   const handleChange = e => {
     const { name, value, checked } = e.target;
     // If the input is checkbox, update the state directly with checked value
@@ -22,54 +25,113 @@ const SignIn = () => {
     setFormData({ ...formData, [name]: name === 'isAdmin' ? checked : value });
   };
 
+  const handleAdmin=()=>{
+   setAdmin(admin?false:true) ;
+   setFormData({...formData,isAdmin:admin?false:true})
+   console.log(admin)
+  }
   const handleSubmit = e => {
     e.preventDefault();
     if (formData.isAdmin) {
-        dispatch(TeachersigninRequest(formData))
-        
+      dispatch(TeachersigninRequest(formData))
+
     } else {
-        
-        dispatch(StudentsigninRequest(formData));
+
+      dispatch(StudentsigninRequest(formData));
     }
   };
 
   return (
-    <div className='container mt-3 mb-3'>
-      <div className='card-body'>
-        <div className='row'>
-          <div className='col'>
+    <div className="row bg-img">
+      {/* This design is for Student Login */}
+      <div className='col-md-4' hidden={admin} >
+        <div className="row d-flex justify-content-center">
+          <div className="d-flex justify-content-center ">
+            <img src={SideImage} className="sideImage mt-8" alt="sideImage" />
+          </div>
+          <div className="d-flex justify-content-center text-white mt-5">
+            <div>
+              <span>
+                <h3 >OnlineExam.com</h3>
+              </span>
+              <span className="d-flex justify-content-center">
+                <h5>Courses | Test Series</h5>
+              </span>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center mt-5">
+            <button className="text-black bg-white loginchangeButton" onClick={handleAdmin}>
+              <h3> Are You admin </h3>
+              <h4>Click Here</h4>
+            </button>
+          </div>
+          <span className="d-flex justify-content-center text-white"> Slogan willbe appear Here</span>
+        </div>
+      </div>
+
+      {/* This is the common design for Student and Teacher */}
+      <div className='col-md-8 bg-white'>
+        <div className='row d-flex justify-content-center h-100'>
+          <div className="col-md-8 signInDetails">
             <form onSubmit={handleSubmit}>
-              <div className='col-4 container form-group'>
-
-              
-                <h3>Sign In</h3>
-                <label htmlFor="email" className="form-label">Email</label>
-                <input type="text" className="form-control" name="email" value={formData.email} onChange={handleChange} placeholder="Enter Your Email" />
+              <div className='mb-5'>
+                <span className="signinHeading">
+                  <h2>Login to your <b>{!admin?"Student":"Admin"} </b>Account</h2>
+                </span>
+                <span className="createNewAccount">
+                  <span>Don't have an account?</span>
+                  <Link> Create One here</Link>
+                </span>
               </div>
-              <div className='col-4 container form-group'>
-                <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Your Password" />
+              <div>
+                <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="Enter Your Email" />
               </div>
-              <div className='col-4 container mt-4 text-center form-group '>
-                <button type="submit" className="btn btn-primary">Sign In</button>
+              <div >
+                <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Your Password" />
               </div>
-
-
-              <div className='col-4 container form-group'>
-                <div className="form-check">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="isAdmin"
-                    name="isAdmin"
-                    checked={formData.isAdmin}
-                    onChange={handleChange}
-                  />
-                  <label className="form-check-label" htmlFor="isAdmin">Are you an admin?</label>
-                </div>
-                </div>
+              <div className='  forgotPassword'>
+                <span><span><input type="checkbox" className="form-check-input checkBox" id="rememberMe" name="rememberMe" /></span><span>Remember Me</span></span>
+                <span><Link>Forgot Password?</Link></span>
+              </div>
+              <div className=' text-center signInButton '>
+                <button type="submit" className="customSignIn-btn">Log In</button>
+              </div>
+              <div className='socialSites'>
+                <span><h4 className="socialSiteMessage">OR TRY OUR SOCIALS</h4></span>
+                <span className="otherSites">
+                  <button className="customSocial-btn">facebook</button>
+                  <button className="customSocial-btn">twitter</button>
+                  <button className="customSocial-btn">google</button>
+                </span>
+              </div>
             </form>
           </div>
+        </div>
+      </div>
+
+      {/* This is for admin Login  */}
+      <div className='col-md-4'  hidden={!admin}>
+        <div className="row d-flex justify-content-center">
+          <div className="d-flex justify-content-center ">
+            <img src={SideImage} className="sideImage mt-8" alt="sideImage" />
+          </div>
+          <div className="d-flex justify-content-center text-white mt-5">
+            <div>
+              <span>
+                <h3 >OnlineExam.com</h3>
+              </span>
+              <span className="d-flex justify-content-center">
+                <h5>Courses | Test Series</h5>
+              </span>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center mt-5">
+            <button className="text-black bg-white loginchangeButton" onClick={handleAdmin}>
+              <h3> Are You Student </h3>
+              <h4>Click Here</h4>
+            </button>
+          </div>
+          <span className="d-flex justify-content-center text-white"> Slogan willbe appear Here</span>
         </div>
       </div>
     </div>
--- src/UserSignUp/Signup.js
+++ src/UserSignUp/Signup.js
@@ -1,18 +1,28 @@
 // SignupForm.js
+import "./Signup.css"
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { signupRequest } from '../actions/userActions';
+import SideImage from "../assets/8.png";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   const dispatch = useDispatch();
+  const [admin, setAdmin] = useState(false)
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
     email: '',
     password: '',
     mobileNumber: '',
-  });
+    password1:'',
+    city:'',
+    country:'',
+    occupation:'',
+    qualification:'',
+    gender:''
 
+  });
 
   const handleChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -21,64 +31,188 @@ const Signup = () => {
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(signupRequest(formData));
+    console.log(formData);
   };
+  const handleAdmin = () => {
+    setAdmin(admin ? false : true)
+  }
 
   return (
+    <div className='row bg-img'>
 
-    <div className='container mt-3 mb-3'>
-      <div className='card-body'>
-        <div className='row'>
-          <h3 className='text-center m-4'>Create Your Account</h3>
-
-
-          <div className='col '>
-            <form onSubmit={handleSubmit} >
-              <div className='col-4 container form-group'>
-                <label htmlFor="firstname" className="form-label">First Name</label>
-                <input type="text" className="form-control" name="firstName" value={formData.firstName} onChange={handleChange} placeholder="Enter your First Name" />
-              </div>
-              <div className='col-4 container form-group'>
-                <label htmlFor="lastname" className="form-label">Last Name</label>
-                <input type="text" className="form-control" name="lastName" value={formData.lastName} onChange={handleChange} placeholder="Enter Your Last Name" />
-              </div>
-              <div className='col-4 container form-group'>
-                <label htmlFor="email" className="form-label">Email</label>
-                <input type="text" className="form-control" name="email" value={formData.email} onChange={handleChange} placeholder="Enter Your Email" />
-              </div>
-              <div className='col-4 container form-group'>
-                <label htmlFor="password" className="form-label">Password</label>
-                <input type="text" className="form-control" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Your Password" />
-              </div>
-
-              <div className='col-4 container form-group'>
-                <label htmlFor="mobileNumber" className="form-label">Mobile Number</label>
-                <input type="number" className="form-control" name="mobileNumber" value={formData.mobileNumber} onChange={handleChange} placeholder="Enter Your Mobile Number" />
-              </div>
-              <div className='col-4 container form-group'>
-                <label htmlFor="role" className="form-label">Role</label>
-                <select className="form-control" name="role" value={formData.role} onChange={handleChange}>
-                  <option value="">Select</option>
-                  <option value="Teacher">Teacher</option>
-                  <option value="Student">Student</option>
-                </select>
-              </div>
-
-              <div className='col-4 container mt-4 text-center form-group '>
-
-                <button type="submit">Sign Up</button>
-              </div>
-
-
-            </form>
-
-
+      {/* This design is for student signup  */}
+      <div className='col-md-4' hidden={admin} >
+        <div className="row d-flex justify-content-center">
+          <div className="d-flex justify-content-center ">
+            <img src={SideImage} className="sideImage mt-8" alt="sideImage" />
           </div>
-
-
+          <div className="d-flex justify-content-center text-white mt-5">
+            <div>
+              <span>
+                <h3 >OnlineExam.com</h3>
+              </span>
+              <span className="d-flex justify-content-center">
+                <h5>Courses | Test Series</h5>
+              </span>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center mt-5">
+            <button className="text-black bg-white loginchangeButton" onClick={handleAdmin}>
+              <h3> Are You admin </h3>
+              <h4>Click Here</h4>
+            </button>
+          </div>
+          <span className="d-flex justify-content-center text-white"> Slogan willbe appear Here</span>
         </div>
-
       </div>
 
+      {/* This signUp from is for Student signup  */}
+      <div className="col-md-8 bg-white">
+        <div className='row d-flex justify-content-center h-100'>
+          <div className="col-md-8 mt-3">
+            <div>
+              <span>
+                <h3>Create Your <b>{admin ? "Teacher" : "Student"}</b> Account</h3>
+              </span>
+              <span className="loginYourAccount">
+                <span>Already Have Account?</span>
+                <Link> Login Here</Link>
+              </span>
+            </div>
+            <div>
+              <form onSubmit={handleSubmit} >
+
+                {/* First Name and Last Name */}
+                <div className="row mt-2">
+                  <div className="col">
+                    <label htmlFor="firstName">First Name*</label>
+                    <input type="text" className="signUpInput"name="firstName" value={formData.firstName} onChange={handleChange} placeholder="Enter your First Name" />
+                  </div>
+                  <div className="col">
+                    <label htmlFor="lastName">Last Name*</label>
+                    <input type="text" className="signUpInput" name="lastName" value={formData.lastName} onChange={handleChange} placeholder="Enter your Last Name" />
+                  </div>
+                </div>
+
+                {/* PhoneNo and Email Id  */}
+                <div className="row ">
+                  <div className="col">
+                    <label htmlFor="phoneNo">PhoneNo*</label>
+                    <input type="number" className="signUpInput" name="mobileNumber" value={formData.mobileNumber} onChange={handleChange} placeholder="Enter your mobile No" />
+                  </div>
+                  <div className="col">
+                    <label htmlFor="emailId">Email Id*</label>
+                    <input type="email" className="signUpInput" name="email" value={formData.email} onChange={handleChange} placeholder="Enter your Email Id" />
+                  </div>
+                </div>
+
+                {/* Password and confirm Password  */}
+                <div className="row ">
+                  <div className="col">
+                    <label htmlFor="password">Password*</label>
+                    <input type="password" className="signUpInput" name="password" value={formData.password} onChange={handleChange} placeholder="Enter your Password" />
+                  </div>
+                  <div className="col">
+                    <label htmlFor="confirmPassword">Confirm Password*</label>
+                    <input type="password" className="signUpInput" name="password1" value={formData.password1} onChange={handleChange} placeholder="Confirm your password" />
+                  </div>
+                </div>
+
+                {/* Country and City */}
+                <div className="row">
+                  <div className="col">
+                    <label htmlFor="country">Country*</label>
+                    <input type="text" className="signUpInput" name="country" value={formData.country} onChange={handleChange} placeholder="Enter your country" />
+                  </div>
+                  <div className="col">
+                    <label htmlFor="city">City*</label>
+                    <input type="text" className="signUpInput" name="city" value={formData.city} onChange={handleChange} placeholder="Enter your city" />
+                  </div>
+                </div>
+
+                {/* Occupation and Qualifications  */}
+                <div className="row ">
+                  <div className="col">
+                    <label htmlFor="gender">Occupation*</label>
+                    <input type="text" className="signUpInput" name="occupation" value={formData.occupation} onChange={handleChange} placeholder="Enter your occupation" />
+                  </div>
+                  <div className="col">
+                    <label htmlFor="qualifications">Qualifications*</label>
+                    <input type="text" className="signUpInput" name="qualification" value={formData.qualification} onChange={handleChange} placeholder="Enter your qualification" />
+                  </div>
+                </div>
+                <div>
+                <div className="col">
+                    <label htmlFor="gender">Gender*</label>
+                    <input type="text" className="signUpInput" name="gender" value={formData.gender} onChange={handleChange} placeholder="Enter your gender" />
+                  </div>
+                  <div className="mt-2">
+                <span><span><input type="checkbox" className="form-check-input checkBox" id="rememberMe" name="rememberMe" /></span><span>I am declearing that All the details provided by me is true and I am only resposible for that</span></span>
+
+                  </div>
+                </div>
+
+
+                {/* <div >
+                  <label htmlFor="lastname" className="form-label">Last Name</label>
+                  <input type="text" className="signUpInput" name="lastName" value={formData.lastName} onChange={handleChange} placeholder="Enter Your Last Name" />
+                </div>
+                 <div >
+                  <label htmlFor="email" className="form-label">Email</label>
+                  <input type="text" className="signUpInput" name="email" value={formData.email} onChange={handleChange} placeholder="Enter Your Email" />
+                </div>
+                <div >
+                  <label htmlFor="password" className="form-label">Password</label>
+                  <input type="text" className="signUpInput" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Your Password" />
+                </div>
+
+                <div > </di
+                  <label htmlFor="mobileNumber" className="form-label">Mobile Number</label>
+                  <input type="number" className="signUpInput" name="mobileNumber" value={formData.mobileNumber} onChange={handleChange} placeholder="Enter Your Mobile Number" />
+               v>
+                <div >
+                  <label htmlFor="role" className="form-label">Role</label>
+                  <select className="signUpInput" name="role" value={formData.role} onChange={handleChange}>
+                    <option value="">Select</option>
+                    <option value="Teacher">Teacher</option>
+                    <option value="Student">Student</option>
+                  </select>
+                </div> */}
+
+                <div className="mt-3">
+                  <button className=" customSignUp-btn"type="submit">Sign Up</button>
+                </div>
+              </form>
+                          </div>
+          </div>
+        </div>
+      </div>
+
+      {/* This design is for admin Signup  */}
+      <div className='col-md-4' hidden={!admin}>
+        <div className="row d-flex justify-content-center">
+          <div className="d-flex justify-content-center ">
+            <img src={SideImage} className="sideImage mt-8" alt="sideImage" />
+          </div>
+          <div className="d-flex justify-content-center text-white mt-5">
+            <div>
+              <span>
+                <h3 >OnlineExam.com</h3>
+              </span>
+              <span className="d-flex justify-content-center">
+                <h5>Courses | Test Series</h5>
+              </span>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center mt-5">
+            <button className="text-black bg-white loginchangeButton" onClick={handleAdmin}>
+              <h3> Are You Student </h3>
+              <h4>Click Here</h4>
+            </button>
+          </div>
+          <span className="d-flex justify-content-center text-white"> Slogan willbe appear Here</span>
+        </div>
+      </div>
     </div>
   );
 };
